ade2ffe905cc5b73f48d6dee633fe200fbd7af21
Konstantinos Spyrou <spyroukon@gmail.com>
2022-09-30 18:41:38 +0300

    changed prod login api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   API_ENDPOINT: '/catalogue',
   REQUESTS_ENDPOINT: '/requests-api',
-  API_LOGIN: '/oauth2/authorization/cite',
+  API_LOGIN: '/oauth2/authorization/bsc',
   projectName: 'Intelcomp'
 };
