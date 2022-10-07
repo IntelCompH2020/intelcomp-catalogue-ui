ca104b5196262352375e5b23f4b11524d71ad890
Antonis Lempesis <antleb@di.uoa.gr>
2022-10-07 13:57:15 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: '/catalogue',
+  API_ENDPOINT: '/api',
   REQUESTS_ENDPOINT: '/requests-api',
   API_LOGIN: '/oauth2/authorization/bsc',
   projectName: 'Intelcomp'
