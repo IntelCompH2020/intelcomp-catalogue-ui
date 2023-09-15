799ad0c684fa4f797626a06630096371a4c9c214
TheQuaker <cs2180008@di.uoa.gr>
2023-09-15 17:33:44 +0300

    adedd enviroment variable logoUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   API_ENDPOINT: '/api',
   REQUESTS_ENDPOINT: '/requests-api',
   API_LOGIN: '/oauth2/authorization/cite',
+  logoURL: 'dataSpaceUI/assets/images/intelcomp-logo.svg',
   projectName: 'Intelcomp'
 };
 
